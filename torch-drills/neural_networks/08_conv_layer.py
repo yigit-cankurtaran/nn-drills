fixed calculation bug

torch-drills/neural_networks/08_conv_layer.py
@@ -24,8 +24,9 @@ def calculate_conv_output_size(input_size, kernel_size, padding=0, stride=1):
     Formula: (input_size + 2*padding - kernel_size) / stride + 1
     """
     # YOUR CODE HERE
-    return (input_size + 2*padding - kernel_size) / stride + 1
-    
+    output =  (input_size + 2*padding - kernel_size) // stride + 1
+    return output    
+
 def solve():
     # Create a simple 5x5 input (like a tiny image)
     # Add batch and channel dimensions: (batch_size=1, channels=1, height=5, width=5)
@@ -51,20 +52,36 @@ def solve():
     # the shape is [1,1,5,5] bc those are batch and channel dimensions, we need it
 
     kernel_size = 3
+    padding = 2
+    stride = 2
 
     conv = nn.Conv2d(in_channels=1,out_channels=1,kernel_size=kernel_size)
-    clone_tensor = conv(clone_tensor)
-    print(f"clone tensor is {clone_tensor} its shape is {clone_tensor.shape}")
-    print(f"our size is {calculate_conv_output_size(input_size=clone_tensor.size(2),kernel_size=kernel_size)}")
+    conv_tensor = conv(clone_tensor)
+    conv_output = calculate_conv_output_size(input_size=clone_tensor.size(2),kernel_size=kernel_size) # passing in height and kernel
+    print(f"conv tensor is {conv_tensor}")
+    if (conv_output == conv_tensor.shape[2]):
+        print("convolution results match")
 
-    conv_with_stride = nn.Conv2d(in_channels=1,out_channels=1,kernel_size=kernel_size,stride=2)
-    clone_tensor = conv_with_stride(clone_tensor)
-    print(f"clone_tensor after stride is {clone_tensor} its shape is {clone_tensor.shape}")
-    print(f"our size is {calculate_conv_output_size(clone_tensor.size(2),kernel_size)}")
+    conv_with_stride = nn.Conv2d(in_channels=1,out_channels=1,kernel_size=kernel_size,stride=stride)
+    stride_tensor = conv_with_stride(clone_tensor)
+    stride_output = calculate_conv_output_size(clone_tensor.size(2),kernel_size=kernel_size,stride=stride)
+    print(f"clone_tensor after stride is {stride_tensor}")
+    if (stride_output == stride_tensor.shape[2]):
+        print("stride results match")
 
-    conv_with_padding = nn.Conv2d(in_channels=1, out_channels=1, kernel_size=kernel_size, padding=2)
-    clone_tensor = conv_with_padding(clone_tensor)
-    print(f"clone_tensor after padding is {clone_tensor} its shape is {clone_tensor.shape}")
+    conv_with_padding = nn.Conv2d(in_channels=1, out_channels=1, kernel_size=kernel_size, padding=padding)
+    padding_tensor = conv_with_padding(clone_tensor)
+    padding_output = calculate_conv_output_size(clone_tensor.size(2),kernel_size=kernel_size,padding=padding)
+    print(f"clone_tensor after padding is {padding_tensor}")
+    if (padding_output == padding_tensor.shape[2]):
+        print("padding results match")
+
+    conv_with_padding_and_stride = nn.Conv2d(in_channels=1, out_channels=1, kernel_size=kernel_size, padding=padding,stride=stride)
+    padding_and_stride_tensor = conv_with_padding_and_stride(clone_tensor)
+    padding_and_stride_output = calculate_conv_output_size(clone_tensor.size(2),kernel_size=kernel_size,stride=stride,padding=padding)    
+    print(f"clone_tensor after padding_and_stride is {padding_and_stride_tensor}")
+    if (padding_and_stride_output == padding_and_stride_tensor.shape[2]):
+        print("padding_and_stride results match")
 
 if __name__ == "__main__":
     solve()