05c8a42c65882b65987c4136dc4277843bcefeaa
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2025-09-01 13:56:22 +0300

    fixed device error, will test

--- torch-drills/debugging/14_memory_leaks.py
+++ torch-drills/debugging/14_memory_leaks.py
@@ -124,7 +124,7 @@ def device():
     if torch.cuda.is_available():
         return torch.device("cuda")
     elif torch.mps.is_available():
-        return torch.device("cuda")
+        return torch.device("mps")
     else:
         return torch.device("cpu")
 
